a2eca9f887c208f29537548c6787daa280bd6e92
Alexander Tih <dgioto@gmail.com>
2021-11-19 22:51:03 +0200

    3 update

--- app/src/main/java/com/dgioto/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/dgioto/criminalintent/CrimeListFragment.kt
@@ -2,12 +2,19 @@ package com.dgioto.criminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment: Fragment() {
+
+    private lateinit var crimeRecycleView: RecyclerView
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this).get(CrimeListViewModel::class.java)
@@ -16,6 +23,18 @@ class CrimeListFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecycleView = view.findViewById(R.id.crime_recycle_view) as RecyclerView
+        crimeRecycleView.layoutManager = LinearLayoutManager(context)
+        return view
     }
 
     companion object{
