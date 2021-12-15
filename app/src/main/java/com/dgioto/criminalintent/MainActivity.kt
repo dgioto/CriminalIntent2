4954265caf5ab021bfc8da02c83c453c5d965153
Alexander Tih <dgioto@gmail.com>
2021-12-15 11:01:47 +0200

    11 update

--- app/src/main/java/com/dgioto/criminalintent/MainActivity.kt
+++ app/src/main/java/com/dgioto/criminalintent/MainActivity.kt
@@ -3,7 +3,7 @@ package com.dgioto.criminalintent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//стр 258
+//стр 266
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
