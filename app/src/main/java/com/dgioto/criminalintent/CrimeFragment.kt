b5f22ae5936b75ef094f4bcfa7f9ef05cd3b4b51
Alexander Tih <dgioto@gmail.com>
2022-01-25 10:47:42 +0200

    fix error with render to fragment_crime

--- app/src/main/java/com/dgioto/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/dgioto/criminalintent/CrimeFragment.kt
@@ -88,24 +88,16 @@ class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
         super.onStart()
 
         val titleWatcher = object : TextWatcher {
-            override fun beforeTextChanged(
-                s: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int) {
-                TODO("Not yet implemented")
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
             }
 
-            override fun onTextChanged(
-                s: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int) {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 crime.title = s.toString()
             }
 
             override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
+
             }
         }
         titleField.addTextChangedListener(titleWatcher)
